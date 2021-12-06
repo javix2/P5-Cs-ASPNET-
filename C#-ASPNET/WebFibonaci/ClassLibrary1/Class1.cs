faa84c85584a9d21724873f02a40b7d45b42f82e
javix2 <91162997+javix2@users.noreply.github.com>
2021-12-06 01:47:41 -0300

    Add files via upload

---
+++ C#-ASPNET/WebFibonaci/ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public String fibonaci(int n) {
+            int a = 0;
+            int b = 1;
+            int f = 0;
+            String r = "";
+            for (int i = 1; i <= n; i++)
+            {
+                f = a + b;
+                b = a;
+                a = f;
+                r = r + f.ToString() + ", ";
+            }
+            return r;
+        }
+    }
+}
